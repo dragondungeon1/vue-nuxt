d686b9667c547447eaa8fde854c5f503cf9b323c
lennart <lennartderidder@gmail.com>
2021-10-01 10:48:07 +0200

    working email

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,75 +1,51 @@
 <template>
-    <div class="container">
-        <form>
-          <label>Name</label>
-          <input 
-            type="text" 
-            v-model="name"
-            name="name"
-            placeholder="Your Name"
-          >
-          <label>Email</label>
-          <input 
-            type="email" 
-            v-model="email"
-            name="email"
-            placeholder="Your Email"
-            >
-          <label>Message</label>
-          <textarea 
-            name="message"
-            v-model="message"
-            cols="30" rows="5"
-            placeholder="Message">
-          </textarea>
-          
-          <input type="submit" value="Send">
-        </form>
-    </div>
+  <div class="container">
+    <form ref="form" @submit.prevent="sendEmail">
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message"></textarea>
+      <input type="submit" value="Send" />
+    </form>
+  </div>
 </template>
 
-
 <script>
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
 export default {
-  name: 'ContactUs',
-  data() {
-    return {
-      name: '',
-      email: '',
-      message: ''
-    }
-  },
   methods: {
-    sendEmail(e) {
-      try {
-        emailjs.sendForm('service_zzi3grf', 'template_j641qge', e.target,
-        'user_xzAl4tedcukr4FdEcS2Wb', {
-          name: this.name,
-          email: this.email,
-          message: this.message
-        })
-
-      } catch(error) {
-          console.log({error})
-      }
-      // Reset form field
-      this.name = ''
-      this.email = ''
-      this.message = ''
+    sendEmail() {
+      emailjs
+        .sendForm(
+          "service_ck7lv1h",
+          "template_j641qge",
+          this.$refs.form,
+          "user_xzAl4tedcukr4FdEcS2Wb"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.text);
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
     },
-  }
-}
+  },
+};
 </script>
 
 <style scoped lang="css">
-
-* {box-sizing: border-box;}
+* {
+  box-sizing: border-box;
+}
 
 .container {
   display: block;
-  margin:auto;
+  margin: auto;
   text-align: center;
   border-radius: 5px;
   background-color: #f2f2f2;
@@ -81,7 +57,9 @@ label {
   float: left;
 }
 
-input[type=text], [type=email], textarea {
+input[type="text"],
+[type="email"],
+textarea {
   width: 100%;
   padding: 12px;
   border: 1px solid #ccc;
@@ -92,8 +70,8 @@ input[type=text], [type=email], textarea {
   resize: vertical;
 }
 
-input[type=submit] {
-  background-color: #4CAF50;
+input[type="submit"] {
+  background-color: #4caf50;
   color: white;
   padding: 12px 20px;
   border: none;
@@ -101,7 +79,7 @@ input[type=submit] {
   cursor: pointer;
 }
 
-input[type=submit]:hover {
+input[type="submit"]:hover {
   background-color: #45a049;
 }
 </style>
